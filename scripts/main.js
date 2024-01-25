83ead63aa9f78c7a949e60345397b36fb280b020
ibabaig <132841966+ibabaig@users.noreply.github.com>
2024-01-25 15:06:54 -0500

    fixed user input issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,41 @@
+// Image switcher code
 
+let myImage = document.querySelector('img');
 
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-
-// Adding an image changer
-const myImage = document.querySelector("img");
-
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute ('src','images/firefox-icon.png');
   }
-};
+}
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  const myName = prompt("Please enter your name.");
-  if (!myName) {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
     setUserName();
   } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
   }
 }
 
-
-if (!localStorage.getItem("name")) {
+if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-//Event handler
-myButton.onclick = () => {
+myButton.onclick = function() {
   setUserName();
-};
-
-
+}
 
 /** let = keyword to declare a variable */
 
